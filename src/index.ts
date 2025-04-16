048805d2752c9a8a2df8062e1b43cc5273f486f6
RNT13 <renatornt13@gmail.com>
2025-04-16 12:21:46 -0300

    Update:  1.3.1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
-import runOptions from '../src/options.js';
-import runSetup from '../src/setup.js';
+
+import runSetup from './setup.js';
+import runOptions from './options.js';
+
+
 
 (async () => {
   await runSetup();
