3d41b9dd5e22b1ef62b0d0d686988d65ed642819
RNT13 <renatornt13@gmail.com>
2025-05-11 12:02:26 -0300

    Update:  1.4.7 (Verção Final)

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ process.chdir(appPath);
 
 // 🎯 Instalando dependências
 console.log("📦 Instalando Dependências de produção...");
-execCommand("npm install styled-components @types/styled-components typescript react-redux @reduxjs/toolkit polished framer-motion react-router-dom react-icons react-input-mask redux@latest @types/react-router-dom --save");
+execCommand("npm install styled-components @types/styled-components typescript react-redux @reduxjs/toolkit polished framer-motion react-router-dom react-icons imask redux@latest @types/react-router-dom --save");
 
 console.log("📦 Instalando Dependências de desenvolvimento...");
 execCommand("npm install eslint eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-prettier prettier eslint-plugin-import eslint-plugin-jsx-a11y eslint-config-prettier vite-plugin-eslint slick-carousel react-slick --save-dev");
